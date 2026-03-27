005b91ab7511b92bced42fd1d856afc468be355d
madbak98 <madbak98@gmail.com>
2026-03-27 14:30:27 +0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,7 @@
-console.log("temp")
+
+  import { createRoot } from "react-dom/client";
+  import App from "./app/App.tsx";
+  import "./styles/index.css";
+
+  createRoot(document.getElementById("root")!).render(<App />);
+  
